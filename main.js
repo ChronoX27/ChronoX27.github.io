f707a69ceb4d5c134cf75e9945d3c2936561266d
ChronoX27 <lu.hauber@web.de>
2023-07-11 15:54:05 +0200

    small js improvements

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.z = 3;
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg'),
-});
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+camera.position.z = 3 + -0.005 * document.body.getBoundingClientRect().top;
+
+
+const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#bg'),});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
 
 // BACKGROUND
 backgroundTexture = new THREE.TextureLoader().load('images/Space_background.jpg');
@@ -29,24 +30,22 @@ scene.add(ambientLight);
 // RANDOM LIGHTS
 function addRandomLights() {
 
-    r = THREE.MathUtils.randInt(0, 255)
-    g = THREE.MathUtils.randInt(0, 255)
-    b = THREE.MathUtils.randInt(0, 255)
-    colorString = 'rgb(' + String(r) + ', ' + String(g) + ', ' + String(b) +')'
+    [r, g, b] = Array(3).fill().map(() => THREE.MathUtils.randInt(-40, 40));
+    colorString = `rgb(${String(r)}, ${String(g)}, ${String(b)})`;
     const color = new THREE.Color(colorString);
 
     const light = new THREE.PointLight(color, 2, 17);
 
     const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randInt(-40, 40));
     light.position.set(x, y, z);
-    
-    //const lightHelper = new THREE.PointLightHelper(light, 0.1, color);
-    scene.add(light) //, lightHelper);
+
+    scene.add(light);
 }
+
 
 // STARS
 function addStar() {
-    const geometry = new THREE.SphereGeometry(THREE.MathUtils.randFloat(0.1, 0.25));
+    const geometry = new THREE.SphereGeometry(THREE.MathUtils.randFloat(0.07, 0.25));
     const material = new THREE.MeshStandardMaterial( {color: 0xFFFFFF} );
     const star = new THREE.Mesh( geometry, material );
 
@@ -55,8 +54,9 @@ function addStar() {
     scene.add(star);
 }
 
- Array(500).fill().forEach(addStar);
- Array(70).fill().forEach(addRandomLights);
+
+Array(600).fill().forEach(addStar);
+Array(75).fill().forEach(addRandomLights);
 
 
 // TORUS
@@ -65,6 +65,7 @@ const material = new THREE.MeshStandardMaterial({ color: 0xECECFF, });
 const torus = new THREE.Mesh(geometry, material);
 torus.position.x = 3
 scene.add(torus);
+
 
 // BOX
 const boxTexture = new THREE.TextureLoader().load('images/Chrono1.png');
@@ -88,30 +89,30 @@ function moveCamera() {
 
     camera.position.z = 3 + t * -0.005;
     camera.position.y = t * 0.0002;
-
   
     pointLight3.position.x = torus.position.x;
-    // lightHelper3.position.x = pointLight3.position.x;
 }
+
 
 document.body.onscroll = moveCamera;
 direction = 1;
-const MAX_ROT = 7300;
+const MAX_ROT = 8000;
 const MIN_ROT = 1700;
 const X_ROT = 1 / Math.floor(Math.random() * (MAX_ROT - MIN_ROT) + MIN_ROT);
 const Y_ROT = 1 / Math.floor(Math.random() * (MAX_ROT - MIN_ROT) + MIN_ROT);
 const Z_ROT = 0.6 / Math.floor(Math.random() * (MAX_ROT - MIN_ROT) + MIN_ROT);
 
+
 function animate() {
     requestAnimationFrame(animate);
 
-// BOX + TORUS ANIMATION
+    // BOX + TORUS ANIMATION
     torus.rotation.y += 0.003;
     box.rotation.x += 0.003;
     box.rotation.y -= 0.003;
     box.rotation.z += 0.003;
  
-// CAMERA ROTATION
+    // CAMERA ROTATION
     // X-ROTATION
     if (camera.rotation.x > 0.2) { direction = -1 }
     else if (camera.rotation.x < -0.17) { direction = 1 }
